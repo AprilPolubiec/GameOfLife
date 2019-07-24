70273784f8e06dacca69c74426738db541520ee1
April Polubiec <aprilpolubiec@gmail.com>
2019-07-24 08:59:22 -0500

    All tests added

--- GOLTests.rb
+++ GOLTests.rb
@@ -1,9 +1,17 @@
 load 'project.rb'
 
-# TODO: there's a lot of repeated code here. Can
-# you move some of into reusable functions to
-# make it shorter and neater?
-
+def runTest(init_board, expected_board, test_number)
+    actual_next_board = nextBoardState(init_board)
+    if actual_next_board == expected_board
+        puts "PASSED " + test_number.to_s
+    else
+        puts "FAILED" + test_number.to_s + "!"
+        puts "Expected:"
+        puts expected_board.map { |x| x.join(' ') }
+        puts "Actual:"
+        puts actual_next_board.map { |x| x.join(' ') }
+    end
+end
 
     # TEST 1: dead cells with no live neighbors
     # should stay dead.
@@ -17,17 +25,8 @@ load 'project.rb'
         [0,0,0],
         [0,0,0]
     ]
-    actual_next_state1 = nextBoardState(init_state1)
 
-    if expected_next_state1 == actual_next_state1
-        print "PASSED 1"
-    else
-        print "FAILED 1!"
-        print "Expected:"
-        print expected_next_state1
-        print "Actual:"
-        print actual_next_state1
-    end
+    runTest(init_state1, expected_next_state1, 1)
 
     # TEST 2: dead cells with exactly 3 neighbors
     # should come alive.
@@ -41,14 +40,79 @@ load 'project.rb'
         [0,1,1],
         [0,0,0]
     ]
-    actual_next_state2 = nextBoardState(init_state2)
+    runTest(init_state2, expected_next_state2, 2)
 
-    if expected_next_state2 == actual_next_state2
-        print "PASSED 2"
-    else
-        print "FAILED 2!"
-        print "Expected:"
-        print expected_next_state2
-        print "Actual:"
-        print actual_next_state2
-    end
+    # TEST 3: live cells with exactly 0 neighbors
+    # should become dead.
+    init_state3 = [
+        [0,1,0],
+        [0,0,0],
+        [1,0,0]
+    ]
+    expected_next_state3 = [
+        [0,0,0],
+        [0,0,0],
+        [0,0,0]
+    ]
+
+    runTest(init_state3, expected_next_state3, 3)
+
+    # TEST 4: live cells with exactly 1 neighbors
+    # should become dead.
+    init_state4 = [
+        [0,1,0],
+        [0,1,0],
+        [0,0,0]
+    ]
+    expected_next_state4 = [
+        [0,0,0],
+        [0,0,0],
+        [0,0,0]
+    ]
+    
+    runTest(init_state4, expected_next_state4, 4)
+
+    # TEST 5: live cells with exactly 2 neighbors
+    # should stay alive.
+    init_state5 = [
+        [1,1,1],
+        [0,0,0],
+        [0,0,0]
+    ]
+    expected_next_state5 = [
+        [0,1,0],            #[0][0] and [0][2] will die because they have just 1 neighbor
+        [0,1,0],            #[1][1] will come alive because it has 3 neighbors   
+        [0,0,0]
+    ]
+    
+    runTest(init_state5, expected_next_state5, 5)
+
+    # TEST 6: live cells with exactly 3 neighbors
+    # should stay alive.
+    init_state6 = [
+        [0,1,1],
+        [0,1,1],
+        [0,0,0]
+    ]
+    expected_next_state6 = [
+        [0,1,1],
+        [0,1,1],
+        [0,0,0]
+    ]
+    
+    runTest(init_state6, expected_next_state6, 6)
+
+    # TEST 7: live cells with more than 3 neighbors
+    # should die.
+    init_state7 = [
+        [1,1,1],
+        [0,1,1],
+        [0,0,0]
+    ]
+    expected_next_state7 = [
+        [1,0,1],
+        [1,0,1],            #[1][0] comes to life because its surrounded by 3
+        [0,0,0]
+    ]
+    
+    runTest(init_state7, expected_next_state7, 7)
